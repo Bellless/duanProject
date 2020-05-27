f606b31f5f4555af5dd6e56442fe772b748d3c17
Bellless <635609622@qq.com>
2020-05-27 16:17:24 +0800

    Signed-off-by: Bellless <635609622@qq.com>项目初始化 Signed-off-by: Bellless <635609622@qq.com>

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// （1）main.js叫这个vue项目程序跑起来时候的'入口文件'
+// （2）也就是说 所有的非vue自带的东西(第三方插件) 都要在这个文件里面注册,然后才能使用
+// （3）main.js文件里面引入（import）的文件 是全局引入（所有的vue页面都能使用引入的东西）
+
+
+// 全局引入淘宝的适配方案
+import 'lib-flexible/flexible.js'
+
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- template这个标签就是用来写我们的html结构的 弹性布局 -->
+  <!-- 有且只有一个div  所有的内容都应该在这个div里面 -->
+  <div class="temp">
+    <div class="content">
+      <div class="logo_div">
+        <div class="logo_div_two">
+          <!-- 旋转logo -->
+          <div class="rotate">
+            <div class="middle">
+              <img src="../assets/img/login/icon/quan1.png" alt="">
+            </div>
+            <div class="small">
+              <img src="../assets/img/login/icon/diqiu.png" alt="">
+            </div>
+            <div class="line-big">
+              <img src="../assets/img/login/icon/quan2.png" alt="">
+            </div>
+          </div>
+        </div>
+        <!-- <p class="logo-div-p">了解行业动态</p> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// 这里面就是用来写js 方法等等的
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
+
+<style lang="stylus" scoped>
+// 局部引入
+@import '../assets/css/common.styl';
+@import '../assets/css/login_icon.css';
+
+/* scoped 表示样式只能在当前页面上生效 */
+/* 这里面是写css的 */
+.temp {
+  position: relative;
+  width: 100%;
+}
+
+.content {
+  position: relative;
+}
+</style>
+
+
